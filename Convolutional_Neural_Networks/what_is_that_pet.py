48608c47c33edff2968e0c05eb90b6e7f2c11e39
armiro <arman.hgh.1374@gmail.com>
2019-01-08 12:34:58 +0330

    add comments

--- Convolutional_Neural_Networks/what_is_that_pet.py
+++ Convolutional_Neural_Networks/what_is_that_pet.py
@@ -9,12 +9,16 @@ classifier = load_model(filepath='trained_model.h5')
 
 img_address = 'dataset/single_prediction/cat_or_dog_2.jpg'
 
-test_image = image.load_img(path=img_address, target_size=(64, 64))
+test_image = image.load_img(path=img_address, target_size=(64, 64), color_mode='rgb')
+# print(test_image.shape)  # it will raise an error because "test_image" is not an array
 test_image = image.img_to_array(img=test_image)
+# print(test_image.shape)  # it will print (64, 64, 3) which means an rgb 64 by 64 image
 test_image = np.expand_dims(a=test_image, axis=0)
-
+# print(test_image.shape)  # it will print (1, 64, 64, 3)
 result = classifier.predict(x=test_image)
 
+# in keras, "predict_proba" is the same as "predict" attribute,
+# seems that there is no way to get the probability value of each prediction
 # prob = classifier.predict_proba(x=test_image, verbose=1)
 # training_set.class_indices
 
